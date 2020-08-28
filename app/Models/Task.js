c29e26633b5a741282c7a13e909827f5e1d3b04b
Nehemiah Boone <nehemiah.boone@gmail.com>
2020-08-28 14:02:10 -0600

    model started

--- app/Models/Task.js
+++ app/Models/Task.js
@@ -1,9 +1,35 @@
+import { generateId } from "../utils.js"
+
 export default class Task {
-  constructor(data) {
-    this.title = data.title
+  constructor({ listName, listItem, addItem, id }) {
+    this.listName = listName
+    this.listItem = listItem
+    this.id = id || generateId()
+    this.addItem = addItem || []
   }
 
   get Template() {
-    return this.title
+    return `
+    <div class="col-4">
+        <div class="card text-white bg-success mb-3">
+          <div class="card-header">${this.listName}</div>
+          <div class="card-body">
+            <ul class="list-group list-group-flush bg-success">
+             ${this.listItemTemplate}
+            </ul>
+          </div>
+        </div>
+      </div>
+    `
+  }
+
+  get listItemTemplate() {
+    let template = ""
+    this.listItem.forEach(l => {
+      template += `
+        <li class="list-group-item">${this.listItem}</li>
+      `
+    })
+    return template
   }
 }
